0ecb848a17db4c0ed06c7253e4eb0300b99e7184
nareesuarezz <naremehs11@gmail.com>
2024-03-06 10:33:20 +0000

    tested database working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,17 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Chrome } from "./Pages/Chrome/Chrome";
 import { Welcome } from "./Pages/Firstpage/Welcome";
+import { Test } from "./backend/test";
+
 function App() {
   return (
     <>
       <main className="backRound"> </main>
-      <Router>
         <Routes>
-          <Route path="/" Component={Chrome} />
-          <Route path="/Welcome" element={Welcome} />
+          <Route path="/" element={<Chrome />} />
+          <Route path="/Welcome" element={<Welcome />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
-      </Router>
     </>
   );
 }
