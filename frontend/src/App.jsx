a3796c396636eebff33eace208fac5bb6c6cb41d
nareesuarezz <naremehs11@gmail.com>
2024-03-07 09:59:31 +0000

    begin from 0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Chrome } from "./Pages/Chrome/Chrome";
 import { Welcome } from "./Pages/Firstpage/Welcome";
-import { Test } from "./backend/test";
-import { Login } from "./backend/auth/auth";
+
 
 
 function App() {
@@ -13,8 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Chrome />} />
           <Route path="/Welcome" element={<Welcome />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/auth" element={<Login />} />
         </Routes>
     </>
   );
