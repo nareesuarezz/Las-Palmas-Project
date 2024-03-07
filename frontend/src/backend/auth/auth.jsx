1dde0e04513d8fcdd567178ff6a3f8de4ce2d365
nareesuarezz <naremehs11@gmail.com>
2024-03-07 08:43:14 +0000

    facebook login (fixing it)

--- frontend/src/backend/auth/auth.jsx
+++ frontend/src/backend/auth/auth.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
+import { Auth, Button, IconFacebook } from '@supabase/ui'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 const supabase = createClient("https://gdovlzckdjkuudotrxob.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkb3ZsemNrZGprdXVkb3RyeG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk3MTQ2NTEsImV4cCI6MjAyNTI5MDY1MX0.hgVrFsLYyVwnggB1eJ9oNPcm1wfZPW3ENpwxuZyFFp8");
@@ -22,8 +22,20 @@ export const Login = () => {
     return () => subscription.unsubscribe()
   }, [])
 
+  const signInWithFacebook = async () => {
+    const { error } = await supabase.auth.signIn({ provider: 'facebook' })
+    if (error) console.log(error.message)
+  }
+
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    return (
+      <div>
+        <Button block size="large" icon={<IconFacebook />} onClick={signInWithFacebook}>
+          Continuar con Facebook
+        </Button>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
+    )
   }
   else {
     return (<div>Logged in!</div>)
