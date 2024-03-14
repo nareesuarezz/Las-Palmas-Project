d95d64fe97c7c3b308f48c86d6fb8f0eab1f4d4f
dkanders12 <dkanders12@gmail.com>
2024-03-14 14:30:38 +0100

    t

--- frontend/src/Components/AvailableTrips/AvailableTrips.jsx
+++ frontend/src/Components/AvailableTrips/AvailableTrips.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./AvailableTrips.scss";
 import { IoCloseOutline } from "react-icons/io5";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 export const AvailableTrips = ({ handleToggle, openIndex, index, route }) => {
   const navigate = useNavigate();
   const handleSeeDetailsClick = () => {
-    navigate('/Details', { state: { route_id: route.route_id } });
+    navigate("/Details", { state: { route_id: route.route_id } });
   };
 
   const isOpen = openIndex === index;
-  console.log("rutas ", route.route_id)
+  console.log("rutas ", route.route_id);
   const date = new Date(route.date);
-  const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+  const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString(
+    [],
+    { hour: "2-digit", minute: "2-digit" }
+  )}`;
 
   return (
-
     <>
-
       <article className="Pick">
         <div className="LeftPick">
           <p>
-
             From {route.fromLocationName} To {route.toLocationName} : <br />
             {route.car.model} <br />
             Time: {formattedDate}
@@ -30,7 +30,6 @@ export const AvailableTrips = ({ handleToggle, openIndex, index, route }) => {
         <div className="rightPick">
           <p
             className={isOpen ? "Active" : "iconDetailsList"}
-
             onClick={() => handleToggle(index)}
           >
             Details
@@ -42,19 +41,23 @@ export const AvailableTrips = ({ handleToggle, openIndex, index, route }) => {
           />
           <ul id="close" className={isOpen ? "DetailsList" : ""}>
             <li className="ShowL" onClick={handleSeeDetailsClick}>
-            <NavLink className="ShowL" to="/Details">
+              <NavLink className="ShowL" to="/Details">
                 See Details
               </NavLink>
             </li>
             <li>
-              <NavLink className="ShowL" to="/WaitingTrips">
+              <NavLink className="ShowL" to="/Chat">
                 Chat
               </NavLink>
             </li>
             <li>
               <NavLink className="ShowL" to="/Map">
-                Back to Map
-
+                Map
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="ShowL" to="/Upcoming">
+                Add
               </NavLink>
             </li>
           </ul>
